a7b4a2d8cccee0e665a043949a7314b7d2725952
Szymon Nastaly <32243444+SzymonNastaly@users.noreply.github.com>
2026-04-20 10:44:31 +0200

    remove pwa, serviceworker

--- app/views/pwa/service-worker.js
+++
@@ -1,8 +0,0 @@
-// No-op service worker. Cleans up any existing caches and unregisters itself.
-self.addEventListener("install", () => self.skipWaiting())
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
-      .then(() => self.registration.unregister())
-  )
-})
